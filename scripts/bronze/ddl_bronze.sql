d3dfbb1b73ef716f2afd9885466896bd868b8812
tatv047 <91747675+tatv047@users.noreply.github.com>
2025-05-01 12:08:44 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,8 +1,18 @@
-
+/*
+========================================================================
+DDL Script : Create Bronze Tables
+========================================================================
+Script Purpose:
+	This script creates tables in the 'brone' schema,dropping existing 
+	tables if they already exist.
+	RUN this script to re-define the DDL structure of 'bronze' tables.
+========================================================================
+*/
 
 -- Creating DDL for all three files from source 'csm'
 IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
+GO
 
 CREATE TABLE bronze.crm_cust_info (
 	cst_id INT,
@@ -14,9 +24,12 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_create_date DATE
 );
 
+GO
+	
 IF OBJECT_ID('bronze.crm_prd_info','U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
-
+GO
+	
 CREATE TABLE bronze.crm_prd_info (
 	prd_id INT,
 	prd_key NVARCHAR(50),
@@ -27,9 +40,13 @@ CREATE TABLE bronze.crm_prd_info (
 	prd_end_dt DATETIME
 );
 
+GO
+	
 IF OBJECT_ID('bronze.crm_sales_details','U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
 
+GO
+	
 CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
@@ -43,27 +60,38 @@ CREATE TABLE bronze.crm_sales_details (
 );
 
 -- Creating DDL for all three files from source 'erp'
-
+GO
+	
 IF OBJECT_ID('bronze.erp_cust_az12','U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
 
+GO 
+	
 CREATE TABLE bronze.erp_cust_az12 (
 	cid NVARCHAR(50),
 	bdate DATE,
 	gen NVARCHAR(50)
 );
 
+GO
+	
 IF OBJECT_ID('bronze.erp_loc_a101','U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
 
+GO
+	
 CREATE TABLE bronze.erp_loc_a101 (
 	cid NVARCHAR(50),
 	cntry NVARCHAR(50)
 );
 
+GO
+	
 IF OBJECT_ID('bronze.erp_px_cat_g1v2','U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
 
+GO
+	
 CREATE TABLE bronze.erp_px_cat_g1v2 (
 	id NVARCHAR(50),
 	cat NVARCHAR(50),
